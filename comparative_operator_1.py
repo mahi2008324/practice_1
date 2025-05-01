39ec7a16a24611ba7c39d6ae9b2d01cf154fb318
mahi2008324 <seshadrinath54054@gmail.com>
2025-05-01 18:55:07 +0530

    comparative_operator

---
+++ comparative_operator_1.py
@@ -0,0 +1,18 @@
+#compartision_operator_greaterthan
+a=[1,2,3,4,5,6]
+b=[6,5,4,3,2,1]
+c=[1,3,5]
+d=[2,4,6]
+print("first one is greater than second one")
+e=a>b
+print("a and b  ",e)
+f=a>c
+print("a and c  ",f)
+g=a>d
+print("a and d  ",g)
+h=b>c
+print("b and c  ",h)
+i=b>d
+print("b and d  ",i)
+j=c>d
+print("c and d  ",j)
