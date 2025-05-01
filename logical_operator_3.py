9004b90223723ddc949b5035d2769ae53118e27e
mahi2008324 <seshadrinath54054@gmail.com>
2025-05-01 19:26:42 +0530

    logical_operator_1

---
+++ logical_operator_3.py
@@ -0,0 +1,16 @@
+a=13
+b=77
+c=93
+d=63
+e=a>=b
+print(not(e))
+f=a>=c
+print("a and c  ",f)
+g=a>=d
+print("a and d  ",g)
+h=b>=c
+print("b and c  ",h)
+i=b>=d
+print("b and d  ",i)
+j=c>=d
+print("c and d  ",j)
