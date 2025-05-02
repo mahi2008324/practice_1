248fc926da9b3d91eebd5dee5d354d0a30ff515b
mahi2008324 <seshadrinath54054@gmail.com>
2025-05-02 12:08:52 +0530

    Add files via upload

---
+++ assignment_operator_7.py
@@ -0,0 +1,13 @@
+#decreaing the given value by 5 multiples
+a=input("enter the value of a :")
+print("the value is  ",a)
+a-=5
+print(a)
+a-=10
+print(a)
+a-=15
+print(a)
+a-=20
+print(a)
+a-=25
+print(a)
